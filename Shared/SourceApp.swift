be72fa0a2eb3a627f693513037b0df58daf9fe8c
kazuki120209 <sk83501229@icloud.com>
2021-05-12 23:38:43 +0900

    Initial Commit

---
+++ Shared/SourceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceApp.swift
+//  Shared
+//
+//  Created by 小路一樹 on 2021/05/12.
+//
+
+import SwiftUI
+
+@main
+struct SourceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
